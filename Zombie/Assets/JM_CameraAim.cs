beb1dbebe548654583d91ab98ef2484b9976d190
Jeremy Martin <jeremymartin1391@hotmail.com>
2014-11-13 05:39:15 -0500

    New Character Model
    Has him walking around and responding to camera movement as his own
    along the horizontal axis, still needs to be straightened out so the
    player can see what they're shooting.

---
+++ Zombie/Assets/JM_CameraAim.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class JM_CameraAim : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
