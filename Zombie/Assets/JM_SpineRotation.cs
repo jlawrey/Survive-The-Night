fd53fd98734f6eba8bbe0a12f6051b75ad2cc173
Jeremy Martin <jeremymartin1391@hotmail.com>
2014-11-19 01:28:07 -0500

    Partial Update (Do Not Start From This)

---
+++ Zombie/Assets/JM_SpineRotation.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class JM_SpineRotation : MonoBehaviour {
+	
+	public float horizontalSpeed; //this is how fast the character turns when the mouse is moved
+
+	// Use this for initialization
+	void Start () 
+	{
+
+	}
+	
+	// Update is called once per frame
+	void LateUpdate ()
+	{
+		float h = horizontalSpeed * Input.GetAxis("Mouse X"); //Custom rotation for the main characters weird spine alignment
+		transform.Rotate(-h, 0, 0);
+
+	}
+}
+
